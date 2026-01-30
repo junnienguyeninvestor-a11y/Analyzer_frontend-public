9421fa02ded253d62487783e71bbb6361b4cdffb
john Doe <your.email@example.com>
2026-01-30 19:11:11 +0300

    addNotInterestedbtn

--- src/component/CardList.js
+++ src/component/CardList.js
@@ -12,6 +12,10 @@ const handleNoReply = (name) => {
   onSubmit(name, 'No reply')
 }
 
+const handleNotInterested = (name) => {
+  onSubmit(name, 'Not interesting/Know scam')
+}
+
 const handleClick = (name) =>{
   if(title==='Accept'){onSubmit(name, 'Chatting')}
   else if(title==='Chatting'){onSubmit(name, 'Waiting')}
@@ -39,6 +43,7 @@ const handleClick = (name) =>{
                     <SimpleCountryClock countries={country} /> 
                     <input type="button" value="done" onClick={()=>handleClick(name)} disabled={loading?true:false}/>
                     <input type="button" value="No Reply" onClick={()=>{handleNoReply(name)}} disabled={loading?true:false}/>
+                    <input type="button" value="Not Interested" onClick={()=>{handleNotInterested(name)}} disabled={loading?true:false}/>
                   </li>
                 );
               })}
